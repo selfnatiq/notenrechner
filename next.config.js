d76ebcdeeb902a9629f3902b2f6dcd5e56592e3a
Mumtaz Natiq, GHR-OSA-NEX-21 <Natiq.Mumtaz@swisscom.com>
2022-06-04 12:38:33 +0200

    add PWA support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+const nextConfig = withPWA({
+	reactStrictMode: true,
+	pwa: {
+		dest: 'public',
+		register: true,
+		skipWaiting: true,
+		runtimeCaching,
+		disable: process.env.NODE_ENV === 'development',
+	},
+})
 
 module.exports = nextConfig
